be7638b652c54a4b339126bf924592d9b93b633d
Hrishikesh Paul <hrishikeshpaul12@gmail.com>
2019-12-29 02:25:59 +0530

    initialc commit

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+
+from brain.label import main
+
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'OPTIONS'])
+@cross_origin(supports_credentials=True)
 def home():
-    return render_template('home.html')
+    res = main(request.get_json())
+    return res
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000, debug=True)
